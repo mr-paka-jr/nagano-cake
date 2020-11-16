59ae217d3cacd006c7b54dd67048b96cb1eb3439
harikakorkmaz <harikanagato@gmail.com>
2020-11-16 05:41:45 +0000

    routes.rb編集

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,32 @@ Rails.application.routes.draw do
   sessions: 'customers/sessions',
   registrations: 'customers/registrations'
 }
+
+
+  root 'customers/items#top'
+  get 'about' => 'customers/items#about'
+
+  scope module: :customers do
+    resources :items,only: [:index,:show]
+    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
+    resources :cart_items,only: [:index,:update,:create,:destroy] do
+      collection do
+        delete '/' => 'cart_items#all_destroy'
+      end
+    end
+    resources :orders,only: [:index,:new,:create,:show] do
+      collection do
+        post 'confirm'
+        get 'thanks'
+      end
+    end
+  end
+  
+  resources :customers,only: [:show, :edit, :update] do
+  	member do
+      get 'withdrawal'
+      patch 'change'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
