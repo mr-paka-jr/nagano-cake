3e56ca23c7a4b18eaf81589a48d142e121bf58cc
EC2 Default User <ec2-user@ip-172-31-67-208.ec2.internal>
2020-11-16 04:34:56 +0000

    genres view修正

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -1,7 +1,6 @@
 class Admins::BaseController < ApplicationController
 
-  before_action :authenticate_admin!
-  
+
   # adminのコントローラー(devise以外)は、views/layouts/application.html.erbではなく、views/layouts/admin.html.erbを参照する
   layout 'admin'
 end
