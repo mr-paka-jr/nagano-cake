e749f625c9c92155dc6e3348663764cf49ea06ae
EC2 Default User <ec2-user@ip-172-31-67-208.ec2.internal>
2020-11-16 11:27:34 +0000

    admins::items新規登録画面作成

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,15 +1,18 @@
 class Admins::ItemsController < Admins::BaseController
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admins_item_path(@item)
+    if @item.save
+      redirect_to admins_item_path(@item)
+    else
+      render 'new'
+    end
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active )
