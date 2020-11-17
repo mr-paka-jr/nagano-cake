8cb7b943ea5e091272e9ee8df9753fcff31d0c5b
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-11-17 05:57:34 +0000

    [add]新規登録バリデーション

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,17 +1,42 @@
 class Customers::AddressesController < ApplicationController
-  
+
   def index
+    @addresses = current_customer.address
+  	@address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+	  @address.customer_id = current_customer.id
+    @addresses = current_customer.address
+    if @address.save
+     redirect_to customers_addresses_path
+    end
   end
-  
+
   def destroy
+    @address = Address.find(params[:id])
+	  @address.destroy
+    @addresses = current_customer.address
   end
-  
+
   def edit
+    @address = Address.find(params[:id])
   end
-  
+
   def update
+    @address = Address.find(params[:id])
+	  if @address.update(address_params)
+     redirect_to customers_addresses_path
+	  else
+	   render "edit"
+	  end
   end
+
+  private
+
+	def address_params
+	  params.require(:address).permit(:name, :address, :postal_code)
+	end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,10 +5,7 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana,
-            :address, :telephone_number,
+            :address, 
             presence: true
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
-  validates :telephone_number, numericality: { only_integer: true }
-  validates :first_name_kana, :last_name_kana,
-      format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "カタカナで入力して下さい。"}
 end
