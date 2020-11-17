ec7c137ad78c7ee2af568cabad22d2ddb541519a
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-11-17 11:14:58 +0000

    [Fix]配送先一覧、編集画面修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #has_many :address, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana,
             :address, presence: true
