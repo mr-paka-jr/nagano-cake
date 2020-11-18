01e4738f67094c9b6c558fe3e00866e5431a376d
EC2 Default User <ec2-user@ip-172-31-67-208.ec2.internal>
2020-11-18 05:05:52 +0000

    会員編集途中

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,10 @@
 class Admins::CustomersController < Admins::BaseController
+
+  def index
+    @customers = Customer.page(params[:id]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
