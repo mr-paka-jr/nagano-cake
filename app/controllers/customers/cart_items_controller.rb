4128705ee9334eaf70cc216b24229c37ce30b3be
harikakorkmaz <harikanagato@gmail.com>
2020-11-18 14:35:55 +0000

    [add]カート商品追加機能、削除機能

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,20 +1,17 @@
 class Customers::CartItemsController < ApplicationController
-  
+
   def index
     @cart_items = current_customer.cart_items
   end
-  
+
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @current_cart_item = Cart_item.find_by(item_id: @cart_item.item_id, customer_id: @cart_item.customer_id)
-    if @current_cart_item = nil?
-      if @cart_item.save
-        redirect_to "/customers/cart_items", notice: "商品が追加されました"
-      else
-        @cart_items = current_customer.carts
-        render 'index'
-      end
+    @current_cart_item = CartItem.find_by(item_id: @cart_item.item_id, customer_id: @cart_item.customer_id)
+    if @cart_item.save
+      redirect_to "/customers/cart_items", notice: "商品が追加されました"
+    else
+      redirect_to "/customers/cart_items", notice: "商品を追加できませんでした"
     end
   end
 
@@ -23,21 +20,21 @@ class Customers::CartItemsController < ApplicationController
     @cart_item.update(cart_item_params)
     redirect_to "/customers/cart_items"
   end
-  
+
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    redirect_to "/customers/cart_items"
+    redirect_to "/customers/cart_items", notice: "商品が削除されました"
   end
-  
-  def destroy_all
-    @cart_item = current_customer.cart_item
+
+  def all_destroy
+    @cart_item = current_customer.cart_items
     @cart_item.destroy_all
     redirect_to "/customers/cart_items", notice: "カートが空になりました"
   end
-  
+
   private
   def cart_item_params
-      params.require(:cart_item).permit(:amount, :item_id)
+      params.require(:cart_item).permit(:customer_id, :amount, :item_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,10 @@ Rails.application.routes.draw do
   namespace :customers do
     resources :items,only: [:index,:show]
     resources :addresses,only: [:index,:create,:edit,:update,:destroy]
-    resources :cart_items,only: [:index,:update,:create,:destroy] do
+    resources :cart_items,only: [:index,:update,:create] do
+      member do
+        delete '/' => 'cart_items#destroy'
+      end
       collection do
         delete '/' => 'cart_items#all_destroy'
       end
