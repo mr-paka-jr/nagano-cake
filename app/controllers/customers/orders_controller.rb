90db6af64f1c422ee0041261bb605726d94b5cdc
harikakorkmaz <harikanagato@gmail.com>
2020-11-19 09:30:41 +0000

    注文商品作成部分できたところまで

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -40,6 +40,20 @@ class Customers::OrdersController < ApplicationController
   end
 
   def create
+    tax = 1.1
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      order_detail = OrderDetail.new
+      order_detail.order_id = @order.id
+      order_detail.item_id = cart_item.item_id
+      order_detail.price = (cart_item.item.price * tax)
+      order_detail.amount = cart_item.amount
+      order_detail.save
+      end
+    @cart_items.destroy_all
+    redirect_to thanks_customers_orders_path
   end
 
   def thanks
