50748fcffe7fde0f6f7b6db9f5500f6946fe3496
harikakorkmaz <harikanagato@gmail.com>
2020-11-20 09:41:29 +0000

    [add]customer注文一覧・注文詳細画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,15 +1,6 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  def customer_total_price
-    sum = 0
-    total_record = self.cart_items
-  end
-  
-  def total_price_commission
-    self.user_total_price + 800
-  end
-  
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
@@ -19,6 +10,7 @@ class Customer < ApplicationRecord
 
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana,
             :address, presence: true
